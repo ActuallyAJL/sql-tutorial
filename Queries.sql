754643790ef2725751ec8ed5733c0b301746947e
ActuallyAJL <zero3260@hotmail.com>
2022-05-31 15:58:01 -0500

    initial commit

--- Queries.sql
+++ Queries.sql
@@ -1,24 +1,25 @@
 ﻿--Query all of the entries in the Genre table
-select * from Genre;
-select * from Album;
+SELECT * FROM Genre;
+SELECT * FROM Album;
+SELECT * FROM Song;
 --Query all the entries in the Artist table and order by the artist's name. HINT: use the ORDER BY keywords
-select * from Artist order by ArtistName;
+SELECT * FROM Artist ORDER BY ArtistName;
 --Write a SELECT query that lists all the songs in the Song table and include the Artist name
-select s.Title, a.ArtistName from Song s left join Artist a on s.ArtistId = a.id;
+SELECT s.Title, a.ArtistName FROM Song s LEFT JOIN Artist a ON s.ArtistId = a.id;
 --Write a SELECT query that lists all the Artists that have a Soul Album
-
+SELECT DISTINCT Artist.ArtistName , Genre.Name FROM Album LEFT JOIN Artist ON Album.ArtistId=Artist.Id LEFT JOIN Genre ON Album.GenreId=Genre.Id WHERE Genre.Id=1;
 --Write a SELECT query that lists all the Artists that have a Jazz or Rock Album
-
+SELECT DISTINCT Artist.ArtistName , Genre.Name FROM Album LEFT JOIN Artist ON Album.ArtistId=Artist.Id LEFT JOIN Genre ON Album.GenreId=Genre.Id WHERE Genre.Id=2 OR Genre.Id=4;
 --Write a SELECT statement that lists the Albums with no songs
-
+SELECT DISTINCT Album.Title , Song.Title FROM Album LEFT JOIN Song ON Song.AlbumId=Album.Id WHERE Song.Title IS NULL;
 --Using the INSERT statement, add one of your favorite artists to the Artist table.
-
+--INSERT INTO Artist (Artistname , YearEstablished) VALUES ('Queens of The Stone Age' , 1996);
 --Using the INSERT statement, add one, or more, albums by your artist to the Album table.
-
+--INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Era Vulgaris', '06/12/2007', 2753, 'Interscope Records', 28, 5);
 --Using the INSERT statement, add some songs that are on that album to the Song table.
-
+--INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Sick, Sick, Sick', 214, '5/08/2007', 5, 28, 23);
 --Write a SELECT query that provides the song titles, album title, and artist name for all of the data you just entered in. Use the LEFT JOIN keyword sequence to connect the tables, and the WHERE keyword to filter the results to the album and artist you added.
-
+SELECT Song.Title , Album.Title , Artist.ArtistName FROM Song LEFT JOIN Album ON Song.AlbumId=Album.Id LEFT JOIN Artist ON Album.ArtistId=Artist.Id WHERE Artist.ArtistName='Queens of The Stone Age';
 --NOTE: Direction of join matters. Try the following statements and see the difference in results.
 
 --SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.Id;
